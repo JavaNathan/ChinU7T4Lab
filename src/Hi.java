c034c6b2608107d884323395f51daa42646fe554
BT_3W2_13 <nathanjchin4@gmail.com>
2024-02-06 09:01:37 -0500

    NEW

--- src/Hi.java
+++ src/Hi.java
@@ -5,9 +5,21 @@ public class Hi {
     public static ArrayList<String> hello (ArrayList<String> hiString){
         ArrayList<String> newList = new ArrayList<String>();
         for (String name : hiString){
-            if (name.indexOf("i") != -1){
+            if (name.contains("i")){
                 newList.add(name);
-                newList.add("hi");
+                newList.add("hi!");
+            }else{
+                newList.add(name);
+            }
+        }
+        return newList;
+    }
+
+    public static ArrayList<String> remove (ArrayList<String> removeString){
+        ArrayList<String> newList = new ArrayList<String>();
+        for (String name : removeString){
+            if (name.length() == 5){
+                newList = newList;
             }else{
                 newList.add(name);
             }
--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,27 @@ import java.util.Arrays;
 public class Main{
     public static void main(String[] args){
         ArrayList<String> names = new ArrayList<String>();
-        names.add("Jessica");
-        names.add("Abby");
-        names.add("Emily");
-        names.add("Destiny");
-        names.add("Mateo");
-        names.add("Sofia");
-        names.add("Tim");
-        names.add("James");
-        names.add("Jim");
+        names.add("Jesse");
+        names.add("Bart");
+        names.add("David");
+        names.add("Abigail");
+        names.add("Frank");
+        names.add("Steph");
+        names.add("Thomas");
+        names.add("Jimmy");
+        names.add("Joe");
+        names.add("Klein");
+        names.add("Daisy");
 
         System.out.println("BEFORE: " + names);
 
-        // write a loop below to traverse names and insert "hi!" after any
-        // name that contains the letter "i"
+        // write a loop below to traverse names and REMOVE each name that
+        // is exactly 5 letters
 
 
-        System.out.println("AFTER: " + Hi.hello(names));
+        System.out.println("AFTER: " + Hi.remove(names));
+
+
 
     }
 }
